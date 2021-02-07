bab78c237e510dd0fe0d980d2921cc7b2cc3ad01
amallya88 <63530138+amallya88@users.noreply.github.com>
2021-02-06 21:16:56 -0500

    added some comments

--- rice university python/collatz_generator.py
+++ rice university python/collatz_generator.py
@@ -8,7 +8,7 @@ def collatz_generator(n):
         yield n  # next value in sequence
 
 
-# return the total stopping time and the sequence
+# return the collatz sequence for n
 def generate_collatz_seq(n):
     if n <= 0:
         raise ValueError("n should be non-zero positive integer")
@@ -17,14 +17,14 @@ def generate_collatz_seq(n):
     while True:
         try:
             nxt_int = next(gen)
-            if nxt_int == 1:
+            if nxt_int == 1:   # stop sequence generation
                 gen.close()
         except StopIteration:
             break
         seq.append(nxt_int)
     return seq
 
-
+# show stop-times for n in [1:1001)
 for n in range(1, 1001):
     collatz_seq = generate_collatz_seq(n)
     print("n={}, stopping time={}".format(n, len(collatz_seq)))
